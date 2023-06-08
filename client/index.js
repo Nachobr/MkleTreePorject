8dd73f32a7c571c4926a1916b252a1e89174b3d3
nachobr <ignbritos@gmail.com>
2023-06-08 16:39:23 -0300

    mrk tree project alch

--- client/index.js
+++ client/index.js
@@ -5,10 +5,18 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Create a Merkle tree using the niceList data
+  const merkleTree = new MerkleTree(niceList);
 
+  // Generate a Merkle proof for your name (replace 'YourName' with the name you want to prove)
+  const nameToProve = 'Rosie Bartell';
+  const merkleProof = merkleTree.getProof(nameToProve);
+
+  // Send the Merkle proof to the server
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: nameToProve,
+    merkleProof: merkleProof,
+    merkleRoot: merkleTree.getRoot().toString('hex')
   });
 
   console.log({ gift });
